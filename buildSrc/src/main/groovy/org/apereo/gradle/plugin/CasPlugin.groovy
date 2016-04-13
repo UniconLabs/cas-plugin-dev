d94c90284b92958d97fb9411a225fee1878c847f
Jj <jsjohnson@unicon.net>
2016-04-13 17:01:05 -0500

    tasks to copy default files and clean up

--- buildSrc/src/main/groovy/org/apereo/gradle/plugin/CasPlugin.groovy
+++ buildSrc/src/main/groovy/org/apereo/gradle/plugin/CasPlugin.groovy
@@ -2,9 +2,13 @@ package org.apereo.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.internal.file.archive.ZipFileTree
 import org.jose4j.jwk.JsonWebKey
 import org.jose4j.jwk.OctJwkGenerator
 import org.springframework.boot.gradle.SpringBootPlugin
+
+import java.security.DigestInputStream
+import java.security.MessageDigest
 
 /**
  * Created by jj on 4/12/16.
@@ -49,12 +53,89 @@ class CasPlugin implements Plugin<Project> {
             }
         }
 
-        project.task('generateKeys') << {
-            println 'Generating keys for CAS'
-            ['tgc.encryption.key': 256, 'tgc.signing.key': 512, 'webflow.encryption.key': 96, 'webflow.signing.key': 512].each { key, size ->
-                def octetKey = OctJwkGenerator.generateJwk(size)
-                def params = octetKey.toParams(JsonWebKey.OutputControlLevel.INCLUDE_SYMMETRIC)
-                println "${key}=${params.get('k')}"
+        project.task('generateKeys') {
+            group = 'CAS'
+            description = 'generate keys for CAS. These keys can be added to your application.properties file'
+            doLast {
+                println 'Generating keys for CAS'
+                ['tgc.encryption.key': 256, 'tgc.signing.key': 512, 'webflow.encryption.key': 96, 'webflow.signing.key': 512].each { key, size ->
+                    def octetKey = OctJwkGenerator.generateJwk(size)
+                    def params = octetKey.toParams(JsonWebKey.OutputControlLevel.INCLUDE_SYMMETRIC)
+                    println "${key}=${params.get('k')}"
+                }
+            }
+        }
+
+        def generateMD5 = { File f ->
+            f.withInputStream {
+                new DigestInputStream(it, MessageDigest.getInstance('MD5')).withStream {
+                    it.eachByte {}
+                    it.messageDigest.digest().encodeHex() as String
+                }
+            }
+        }
+
+        project.task('copyCasResources') {
+            group = 'CAS'
+            description = 'copy the resources from the CAS distribution'
+            doLast {
+                println "copying resources from CAS"
+                def resourceRoot = project.file('src/main/resources')
+                if (!resourceRoot) {
+                    project.file('src/main/resources').mkdirs()
+                }
+                def resource = project.configurations.compile.find {
+                    it.name.matches('cas-server-webapp-.*-resources\\.jar')
+                }
+                println "found: ${resource}"
+                project.copy {
+                    from project.zipTree(resource)
+                    into temporaryDir
+                }
+                project.fileTree(temporaryDir).visit { el ->
+                    if (!el.file.isDirectory()) {
+                        println "checking ${el.relativePath} (${generateMD5 el.file})"
+                        if (!project.file("${resourceRoot}/${el.relativePath}").exists() || (generateMD5(el.file) != generateMD5(project.file("${resourceRoot}/${el.relativePath}")))) {
+                            project.copy {
+                                from el.file
+                                into project.file("${resourceRoot}/${el.relativePath}").parent
+                                if (project.file("${resourceRoot}/${el.relativePath}").exists() && (generateMD5(el.file) != generateMD5(project.file("${resourceRoot}/${el.relativePath}")))) {
+                                    rename {String fileName ->
+                                        "${fileName}.casorig"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        project.task('cleanCasResources') {
+            group = 'CAS'
+            description = 'remove default resources from tree'
+            doLast {
+                println 'cleaning up resources from CAS'
+                project.copy {
+                    from project.zipTree(project.configurations.compile.find { it.name.matches('cas-server-webapp-.*-resources\\.jar')})
+                    into temporaryDir
+                }
+                //clean up casorig files
+                project.fileTree('src/main/resources').matching {
+                    include '**/*.casorig'
+                }.each {
+                    it.delete()
+                }
+                // clean up files that haven't changed
+                project.fileTree('src/main/resources').visit { el ->
+                    if (!el.file.isDirectory()) {
+                        println "checking ${el.relativePath}"
+                        def orig = project.file("${temporaryDir}/${el.relativePath}")
+                        if (orig.exists() && generateMD5(orig) == generateMD5(el.file)) {
+                            el.file.delete()
+                        }
+                    }
+                }
             }
         }
     }
